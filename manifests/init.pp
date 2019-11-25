ae3f2ad730aaa6d267fbabed070aae3dafe7f739
Alexander Reviakin <reviakin@gmail.com>
2019-11-25 10:53:43 +0500

    Настраиваемый через Puppet-класс конфиг-файл

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,20 @@
-class spoo_inv {
+class spoo_inv (
+	$apihost='inventory.domain.local',	#где находится сервер инвентаризации
+	$logfile='/var/log/inventory.log',	#куда логировать работу скрипта
+	$cronmin='*/47'				#расписание для крона (минуты)
+){
+	$config="apihost=${apihost}\nlogfile=${logfile}"
 	file {'/usr/local/etc/inventory/':
 		ensure	=> directory,
 		source	=> 'puppet:///modules/spoo_inv',
 		recurse	=> true,
 	} ->
+	file {'/usr/local/etc/inventory/priv.conf.sh':
+		content	=> $config
+	} ->
 	cron {'inventory update':
 		command	=>  '/usr/local/etc/inventory/inventory.sh',
 		user	=>  root,
-		minute	=> '*/47',
+		minute	=> $cronmin,
 	}
 }
-
