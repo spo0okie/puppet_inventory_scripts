d0c44384553f04fc3ac5d784cc717ae422755e24
root <root@home.reviakin.net>
2023-02-14 09:53:08 +0500

    v0.6.4

--- files/fn.hwjson.sh
+++ files/fn.hwjson.sh
@@ -20,19 +20,54 @@ else
 	
 fi
 
-for drive in `lsblk -dnrb | grep disk | cut -d" " -f1`; do
-	if [ -z "$1" ]; then
-		drive_model=`smartctl -i /dev/$drive | grep "Device Model" | cut -d':' -f2| sed -e 's/^[[:space:]]*//'`
-		drive_sn=`smartctl -i /dev/$drive | grep "Serial Number" | cut -d':' -f2| sed -e 's/^[[:space:]]*//'`
-	else
-		drive_model=Virtual
-		drive_sn=
-	fi
-	drive_capacity=`lsblk -dnrb |grep $drive | cut -d" " -f4`
-	drive_size=$(( $drive_capacity / 1073741824 ))
-	echo ","
-	echo "{\"harddisk\": {\"model\":\"$drive_model\",\"size\":\"$drive_size\",\"serial\":\"$drive_sn\"}}"
-done
+
+
+if [ -z "$(which lsblk)" ]; then
+	#наверно этот вариант более универсальный, но появилсся позже, когда пришлось работать с машинами без lsblk
+	#поэтому пока используем как план Б на случай отсутствия основного инструмента, чтобы вдруг не отвалились данные на куче машин (толком не протестирована соместимость же)
+	lshw_tmp=`mktemp`
+	lshw -c disk > $lshw_tmp
+	lshw_drives=`cat $lshw_tmp| grep -Pzo "\*-(disk|namespace)(:\d+)\n" | tr "\0" "\n"`
+
+	for drive in $lshw_drives; do
+		drive_size=$( cat $lshw_tmp | grep -Pzo "\\$drive?(\n.*)+?\s+size:\s+\K\d+\w+" | tr "\0" "\n" )
+		drive_model=$( cat $lshw_tmp | grep -Pzo "\\$drive?(\n.*)+?\s+product:\s+\K.*\n" | tr "\0" "\n" )
+		drive_sn=$( cat $lshw_tmp | grep -Pzo "\\$drive?(\n.*)+?\s+serial:\s+\K.*\n" | tr "\0" "\n" )
+		#"
+		#TiB->GiB<-MiB<-KiB
+		if echo $drive_size | grep -q KiB; then
+			drive_size=$(( `echo $drive_size | tr -d "KiB"` / 1024 ))MiB
+		fi
+		if echo $drive_size | grep -q MiB; then
+			drive_size=$(( `echo $drive_size | tr -d "MiB"` / 1024 ))
+		fi
+		if echo $drive_size | grep -q TiB; then
+			drive_size=$(( `echo $drive_size | tr -d "TiB"` * 1024 ))
+		fi
+		if echo $drive_size | grep -q GiB; then
+			drive_size=`echo $drive_size | tr -d "GiB"`
+		fi
+		echo ","
+		echo "{\"harddisk\": {\"model\":\"$drive_model\",\"size\":\"$drive_size\",\"serial\":\"$drive_sn\"}}"
+	done
+
+	rm -f $lshw_tmp
+else
+	#легаси вариант
+	for drive in `lsblk -dnrb | grep disk | cut -d" " -f1`; do
+		if [ -z "$1" ]; then
+			drive_model=`smartctl -i /dev/$drive | grep "Device Model" | cut -d':' -f2| sed -e 's/^[[:space:]]*//'`
+			drive_sn=`smartctl -i /dev/$drive | grep "Serial Number" | cut -d':' -f2| sed -e 's/^[[:space:]]*//'`
+		else
+			drive_model=Virtual
+			drive_sn=
+		fi
+		drive_capacity=`lsblk -dnrb |grep $drive | cut -d" " -f4`
+		drive_size=$(( $drive_capacity / 1073741824 ))
+		echo ","
+		echo "{\"harddisk\": {\"model\":\"$drive_model\",\"size\":\"$drive_size\",\"serial\":\"$drive_sn\"}}"
+	done
+fi
 
 
 # вот это вот дело законменчено, т.к. почему-то линцукс говорит что у меня памяти меньше чем на самом деле
